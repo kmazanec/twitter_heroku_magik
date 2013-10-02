7b22b9b3068da5564ab010e8e2e43a97656d992e
kmazanec <k.mazanec@gmail.com>
2013-10-01 22:15:08 -0500

    added some style

--- public/js/application.js
+++ public/js/application.js
@@ -6,12 +6,17 @@ Tweet.prototype.toHTML = function() {
   return "<li>"+this.text+"</li>";
 };
 
+function removeMessage (){
+  $("#response_message").empty();
+}
 
 
 $(document).ready(function() {
 
   $("#post_new_tweet").on("submit", function(event){
     event.preventDefault();
+    $("#waiting").show();
+
     url = $(this).attr("action");
     console.log(url);
     data = $(this).serialize();
@@ -19,13 +24,17 @@ $(document).ready(function() {
 
     new_tweet = new Tweet($(this).find("#new_tweet").val());
 
-    
-    $.post(url, data, function(response){
-      $("#response_message").text(response);
-      $("#post_new_tweet").trigger("reset");
-      $("#recent_tweets ul").append(new_tweet.toHTML());
-    });
-
+    if (new_tweet.length() > 140 ) {
+      $("#response_message").text("Your tweet must be less than 140 chars, newb!");
+    } else {
+      $.post(url, data, function(response){
+        $("#response_message").text(response);
+        $("#post_new_tweet").trigger("reset");
+        $("#recent_tweets ul").append(new_tweet.toHTML());
+        $("#waiting").hide();
+      });
+    }
+    window.setTimeout(removeMessage, 3000);
   });
 
 });
